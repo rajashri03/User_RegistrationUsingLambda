30b2966e23b85742544585559824a8bc0e42e3c9
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-26 15:13:20 +0530

    Add:Validate First Name using Regex and Custom Exception

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UserRegistration_CustomException;
+
+/// <summary>
+/// Check First Name is valid or not
+/// </summary>
+UserRegistration user = new UserRegistration();
+Console.WriteLine("Enter name");
+string fname = Console.ReadLine();
+string result = user.FirstNameMatch(fname);
+Console.WriteLine(result);
